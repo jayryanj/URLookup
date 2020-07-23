3b949dc317ad3cc01bbd03e793f182ed8a8db0e2
jayryanj <jayryanj@hawaii.edu>
2020-07-23 10:40:57 -1000

    Update scanner.js
    Started implemention for urlscanner.io API

--- services/scanner.js
+++ services/scanner.js
@@ -6,35 +6,73 @@ const Keys = require('../config/keys');
 const version = require('../package.json').version;
 const bodyParser = require('body-parser');
 
-const testURL = "testsafebrowsing.appspot.com/s/phishing.html"
+const testURL = "testsafebrowsing.appspot.com/s/phishing.html" // Only for testing
 
 function scan(url) {
-    const data = {
-        "client": {
-            "clientId":      "URLooker",
-            "clientVersion": `${version}`
-          },
-          "threatInfo": {
-            "threatTypes": [
-                "MALWARE",
-                "SOCIAL_ENGINEERING",
-                "POTENTIALLY_HARMFUL_APPLICATION",
-                "UNWANTED_SOFTWARE"
-            ],
-            "platformTypes": ["ANY_PLATFORM"],
-            "threatEntryTypes": ["URL"],
-            "threatEntries": [
-              {"url": `${url}`}
-            ]
-          }
-    }
-
-    return axios.post(`https://safebrowsing.googleapis.com/v4/threatMatches:find?key=${Keys.gsbKey}`, data)
-      .then((results) => { return results.data });
+  
+  return submitAPI(url)
+    .then((url) => {
+      return resultsAPI(url)
+    })
+    .catch(error => console.log(error))
 }
 
+/*
 function googleSafeBrowsing(url) {
-    // Test
+  const data = {
+    "client": {
+        "clientId":      "URLooker",
+        "clientVersion": `${version}`
+      },
+      "threatInfo": {
+        "threatTypes": [
+            "MALWARE",
+            "SOCIAL_ENGINEERING",
+            "POTENTIALLY_HARMFUL_APPLICATION",
+            "UNWANTED_SOFTWARE"
+        ],
+        "platformTypes": ["ANY_PLATFORM"],
+        "threatEntryTypes": ["URL"],
+        "threatEntries": [
+          {"url": `${url}`}
+        ]
+      }
+}
+  return axios.post(`https://safebrowsing.googleapis.com/v4/threatMatches:find?key=${Keys.gsbKey}`, data)
+    .then((results) => { return results.data });
+}*/
+
+function submitAPI(url) {
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+      "API-Key": `${Keys.urlscanKey}`
+    }
+  };
+
+  const data = {
+    "url": url,
+    "visibility": "unlisted"
+  };
+
+  return axios.post("https://urlscan.io/api/v1/scan/", data, config)
+    .then((response) => {
+      return response.data.api;
+     })
+    .catch(error => {
+      console.log("Oh no!");
+      console.log(error);
+    })
+}
+
+function resultsAPI(url) {
+  return axios.get(url)
+    .then(response => { 
+      return response.data 
+    })
+    .catch( error => {
+      console.log(error);
+    });
 }
 
 module.exports = scan;
